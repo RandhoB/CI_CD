fae23b848f2f8e89fca3cf373f5955a9f30ee1d1
RandhoB <103103621+RandhoB@users.noreply.github.com>
2022-05-26 08:10:11 +0700

    Update main.go

--- main.go
+++ main.go
@@ -8,5 +8,6 @@ import (
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
 	log.Println("listening on port 3000...")
+	log.Println("belajar gaes percobaan cicd 1")
 	http.ListenAndServe(":3000", nil)
 }
